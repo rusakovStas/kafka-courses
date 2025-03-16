abeb6c7752651104eaa5bb03b61be5a0fb761129
Станислав Русаков <rusakof.stas@gmail.com>
2025-03-16 16:18:22 +0300

    кажется заработало

--- lesson_2/app/src/main/java/ydx/practicum/app/config/MessageCense.java
+++ lesson_2/app/src/main/java/ydx/practicum/app/config/MessageCense.java
@@ -1,15 +1,18 @@
 package ydx.practicum.app.config;
 
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.processor.api.Processor;
 import org.apache.kafka.streams.processor.api.ProcessorContext;
 import org.apache.kafka.streams.processor.api.Record;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.ValueAndTimestamp;
 import org.springframework.stereotype.Component;
 import ydx.practicum.app.model.Message;
 
 import java.util.Set;
+import java.util.function.Consumer;
 
 @Component
 public class MessageCense {
@@ -26,25 +29,24 @@ public class MessageCense {
     public KStream<String, Message> censeMessage(KStream<String, Message> stream) {
         stream.process(() -> new Processor<String, Message, String, Message>() {
             private ProcessorContext<String, Message> context;
-            private KeyValueStore<String, Set<String>> store;
+            private KeyValueStore<String, ValueAndTimestamp<Set<String>>> store;
 
             @Override
             public void init(ProcessorContext<String, Message> context) {
                 this.context = context;
-                this.store = context.getStateStore(kafkaProperties.getStoreForBlacklistWord());
+                //  этот ValueAndTimestamp - вообще не понятно откуда вылез
+                this.store = (KeyValueStore<String, ValueAndTimestamp<Set<String>>>) context.getStateStore(kafkaProperties.getStoreForBlacklistWord());
             }
 
             @Override
             public void process(Record<String, Message> record) {
-                // TODO: не работает. Уже не понимаю почему
-                Set<String> bannedWords = store.get(kafkaProperties.getKeyForBlacklistWords());
                 Message message = record.value();
-                bannedWords.forEach((word) -> {
+                store.all().forEachRemaining(stringSetKeyValue -> stringSetKeyValue.value.value().forEach((word) -> {
                     message.setMessage(message.getMessage().replace(word, "***"));
-                });
+                }));
                 context.forward(record.withValue(message));
             }
-        });
+        }, kafkaProperties.getStoreForBlacklistWord());
         return stream;
     }
 }
